817a6a503a5b334e007d261bdf54612d791f12aa
szonavy <viktoriaszotaknagy@gmail.com>
2023-10-07 13:27:19 -0400

    practicing loop (for)

---
+++ src/main/java/practice/basicexcercise/_03_loop/MainLoop.java
@@ -0,0 +1,10 @@
+package practice.basicexcercise._03_loop;
+
+public class MainLoop {
+    public static void main(String[] args) {
+        int N = 2;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+    }
+}
