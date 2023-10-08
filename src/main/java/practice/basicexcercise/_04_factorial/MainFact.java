c026415b7a33e1a13926177b17f8547174fa2989
szonavy <viktoriaszotaknagy@gmail.com>
2023-10-08 15:59:05 -0400

    create a number of factorial

---
+++ src/main/java/practice/basicexcercise/_04_factorial/MainFact.java
@@ -0,0 +1,23 @@
+package practice.basicexcercise._04_factorial;
+
+import java.util.Scanner;
+
+public class MainFact {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please give me a number: ");
+        int n = sc.nextInt();
+        int result = 1;
+        System.out.println(n + "! = ");
+
+        for(int i = n; i >= 1; i--){
+            System.out.println(i);
+            result *= i;
+        }
+        System.out.println("Result: " + result);
+    }
+
+
+
+}
