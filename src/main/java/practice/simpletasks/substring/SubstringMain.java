39f76b96a1f7e162e384901000cefbab24b28795
szonavy <viktoriaszotaknagy@gmail.com>
2023-10-26 16:17:56 -0400

    Solving substring task

---
+++ src/main/java/practice/simpletasks/substring/SubstringMain.java
@@ -0,0 +1,14 @@
+package practice.simpletasks.substring;
+
+import java.util.Scanner;
+
+public class SubstringMain {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }
+}
