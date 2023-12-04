37beded4cde15c1fcce3a8163d18c08e2f253fd2
szonavy <viktoriaszotaknagy@gmail.com>
2023-12-04 15:27:18 -0500

    The tv series task is in progress.

--- src/main/java/practice/complextask/tvseries/TvSeriesManager.java
+++ src/main/java/practice/complextask/tvseries/TvSeriesManager.java
@@ -1,7 +1,6 @@
 package practice.complextask.tvseries;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TvSeriesManager {
     public static void main(String[] args) {
@@ -59,10 +58,69 @@ public class TvSeriesManager {
 
         System.out.println("The user spent " + day + " days " +hr + " hours " + min + " minutes with watching the tv series.");
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please five me a date as the following format:  yyyy.mm.dd ");
+        String givenNumber = sc.next();
+
+        List<TvSeriesData> notSeenSeries = new ArrayList<>();
+
+        String w = givenNumber.replaceAll("\\.","");
+        System.out.println(w);
+        int r = Integer.parseInt(givenNumber.replaceAll("\\.", ""));
+        for(TvSeriesData da : data){
+            if(da.watched == 0 && !da.date.equals("NI")) {
+                int s = Integer.parseInt(da.date.replaceAll("\\.", ""));
+                if(s <= r){
+                    notSeenSeries.add(da);
+                }
+            }
+        }
+        System.out.println("\nTask 5:");
+
+        for(TvSeriesData d: notSeenSeries){
+            System.out.println(d.seasonEpisode + "  " + d.name);
+        }
+
+        System.out.println("\nTask 7:");
+
+        System.out.println("Please give me a day of the week: like 'cs'");
+        Set<String> seriesOnTheSameDay = new HashSet<>();
+
+        String reqDay = sc.next();
+
+        for(TvSeriesData date: data) {
+            if (!date.date.equals("NI")) {
+                String[] cutDate = date.date.split("\\.");
+                String dw = daysOfTheWeek(Integer.parseInt(cutDate[0]), Integer.parseInt(cutDate[1]), Integer.parseInt(cutDate[2]));
+
+                if (reqDay.equals(dw)) {
+                    seriesOnTheSameDay.add(date.name);
+                }
+            }
+        }
+        if(seriesOnTheSameDay.isEmpty()){
+            System.out.println("There weren't any tv series on that day.");
+        }else{
+            System.out.println(seriesOnTheSameDay);
+        }
+
+        System.out.println("\nTask 8:");
+
 
 
     }
 
+    public static String daysOfTheWeek(int y, int m, int d){
+        String days[] = {"v","h","k","sze","cs","p","szo"};
+        int[] months = {0,3,2,5,0,3,5,1,4,6,2,4};
+
+        if(m < 3) {
+            y = y-1;
+        }
+        String result = days[(y + y / 4 - y /100 + y / 400 + months[m-1] + d) % 7];
+        return result;
+    }
 
 
 }
