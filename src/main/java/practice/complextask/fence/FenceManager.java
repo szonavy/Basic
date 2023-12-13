cfd9c0299f9991ec97fc058efcb2ee4058b5f78e
szonavy <viktoriaszotaknagy@gmail.com>
2023-12-13 12:19:09 -0500

    The fence task is in progress- 4,5 tasks are solved.

--- src/main/java/practice/complextask/fence/FenceManager.java
+++ src/main/java/practice/complextask/fence/FenceManager.java
@@ -2,8 +2,7 @@ package practice.complextask.fence;
 
 import practice.a_common.MyFileReader;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FenceManager {
     public static void main(String[] args) {
@@ -38,6 +37,49 @@ public class FenceManager {
         }
         System.out.println("The details about the last house are: There is on the " + eOrO + " side of the street." );
         System.out.println("The house number of the last house is : " + lastHouse.houseNumber);
+
+        System.out.println("\nTask 4:");
+        List<FenceData> oddStreet = new ArrayList<>();
+        for(int i = 1; i < data.size()-1; i++){
+            if(data.get(i).oddOrEven == 1){
+                oddStreet.add(data.get(i));
+            }
+        }
+        for(int i = 1; i < oddStreet.size()-1; i++){
+            if(oddStreet.get(i-1).fenceColor.equals(oddStreet.get(i).fenceColor) && oddStreet.get(i).fenceColor.equals(oddStreet.get(i+1).fenceColor)){
+                System.out.println(data.get(i-1).fenceColor +" " + data.get(i).fenceColor + " " + data.get(i+1).fenceColor + " " + data.get(i).houseNumber);
+            }
+        }
+
+        System.out.println("\nTask 5:");
+        Scanner sc = new Scanner(System.in);
+        int requestedHouseNumber = sc.nextInt();
+        FenceData neighbour1;
+        FenceData neighbour2;
+        FenceData owner;
+
+        for(int i = 0; i < data.size(); i++){
+            if(data.get(i).houseNumber == requestedHouseNumber){
+                neighbour1 = data.get(i-1);
+                owner = data.get(i);
+                neighbour2 = data.get(i+1);
+                System.out.println(wichColour(neighbour1.fenceColor,owner.fenceColor,neighbour2.fenceColor));
+
+            }
+        }
+    }
+
+    static String wichColour(String ne1Color, String ownerColor, String n2Color){
+        String result = "";
+
+        for(int i = 'A' ; i <= 'Z'; i++){
+            String ch = (char)i+"";
+            if(!ne1Color.equals(ch) && !ownerColor.equals(ch) && !n2Color.equals(ch)){
+                result = (char)i+" ";
+                break;
+            }
+        }
+        return result;
     }
 
 
