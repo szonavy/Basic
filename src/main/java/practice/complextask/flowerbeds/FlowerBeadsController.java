8e95a874062d3076c753d11ba0e7c9edd4fd673e
szonavy <viktoriaszotaknagy@gmail.com>
2024-02-20 09:22:35 -0500

    The flowerbed task is done.

--- src/main/java/practice/complextask/flowerbeds/FlowerBeadsController.java
+++ src/main/java/practice/complextask/flowerbeds/FlowerBeadsController.java
@@ -1,16 +1,17 @@
 package practice.complextask.flowerbeds;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FlowerBeadsController {
     List<FlowerBedsData> data = new ArrayList<>();
+    int numberOfFlowers = 0;
 
     public FlowerBeadsController(List<String> rows) {
         fileHandling(rows);
     }
     private void fileHandling(List<String> rows){
+        numberOfFlowers = Integer.parseInt(rows.get(0));
         for(int i = 1; i < rows.size(); i++){
             String[] cuttingRows = rows.get(i).split(" ");
 
@@ -26,5 +27,131 @@ public class FlowerBeadsController {
                 .filter(l->l.startBedLocation > l.endBedLocation)
                 .map(m->m.id)
                 .collect(Collectors.toList());
+    }
+
+    public int countOffersIncludedBed(int bedId){
+
+        return (int) data.stream()
+                .filter(l->l.startBedLocation > l.endBedLocation && l.startBedLocation >= bedId && l.endBedLocation >= bedId || l.startBedLocation < l.endBedLocation && l.startBedLocation <= bedId && l.endBedLocation >= bedId)
+                .mapToInt(l->l.id)
+                .count();
+    }
+
+    public String getTheColorOfTheRequestedBedByTheFirstOffer(int bedId){
+
+        return data.stream()
+                .filter(l->l.startBedLocation > l.endBedLocation && l.startBedLocation >= bedId && l.endBedLocation >= bedId || l.startBedLocation < l.endBedLocation && l.startBedLocation <= bedId && l.endBedLocation >= bedId)
+                .map(k->k.color)
+                .findFirst()
+                .orElse("This bed is not planted.");
+    }
+
+    public Set<String> getTheColorOfTheRequestedBedByAllOffer(int bedId){
+        return data.stream()
+                .filter(l->l.startBedLocation > l.endBedLocation && l.startBedLocation >= bedId && l.endBedLocation >= bedId || l.startBedLocation < l.endBedLocation && l.startBedLocation <= bedId && l.endBedLocation >= bedId)
+                .map(k->k.color)
+                .collect(Collectors.toSet());
+    }
+
+    private int getTheNumberOfBedsInTheOffers(){
+        int count = 0;
+        for (FlowerBedsData d : data) {
+            if(d.startBedLocation < d.endBedLocation){
+                count += d.endBedLocation - d.startBedLocation;
+            }else{
+                count += numberOfFlowers - d.startBedLocation + d.endBedLocation;
+            }
+        }
+        return count;
+    }
+
+    private Set<Integer> generateTheFlowerbeds(){
+        Set<Integer> flowerBedsById = new HashSet<>();
+
+        for(int i = 1; i <= numberOfFlowers; i++){
+            flowerBedsById.add(i);
+        }
+        return flowerBedsById;
+    }
+
+    private Set<Integer> deleteTheUsedBeds(){
+        Set<Integer> flowerBedsById = generateTheFlowerbeds();
+
+        for(FlowerBedsData d : data){
+            if(d.startBedLocation < d.endBedLocation){
+                for(int i = d.startBedLocation; i <= d.endBedLocation; i++){
+                    flowerBedsById.remove(i);
+                }
+            }else{
+                for(int i = d.startBedLocation; i <= numberOfFlowers; i++){
+                    flowerBedsById.remove(i);
+                }
+                for(int i = 1; i <= d.endBedLocation; i++){
+                    flowerBedsById.remove(i);
+                }
+            }
+        }
+        return flowerBedsById;
+    }
+
+    public String isPossibleToPlantAllBeds(){
+        String message = "";
+        int sizeOfRestBed = deleteTheUsedBeds().size();
+        int numberOfAllOffersByBeds = getTheNumberOfBedsInTheOffers();
+
+        if(numberOfAllOffersByBeds > numberOfFlowers && sizeOfRestBed > 0){
+            message = "Implantation can be solved by reorganization.";
+        }else if(sizeOfRestBed == 0){
+            message = "There are applicants for planting every bed.";
+        }else if(numberOfAllOffersByBeds < numberOfFlowers){
+            message = "The implantation cannot be solved.";
+        }
+        return message;
+    }
+
+    private Map<Integer,Map<String,Integer>> getTheColorOfTheBeds(){
+        Map<Integer,Map<String,Integer>> colorBed = new HashMap<>();
+        Map<String,Integer> col = new HashMap<>();
+
+        for(FlowerBedsData d : data){
+            if(d.startBedLocation < d.endBedLocation){
+                for(int i = d.startBedLocation; i <= d.endBedLocation; i++){
+                    if(!colorBed.containsKey(i)){
+                        col.put(d.color,d.id);
+                        colorBed.put(i,col);
+                        col = new HashMap<>();
+                    }
+                }
+            }else{
+                for(int i = d.startBedLocation; i <= numberOfFlowers; i++){
+                    if(!colorBed.containsKey(i)){
+                        col.put(d.color,d.id);
+                        colorBed.put(i,col);
+                        col = new HashMap<>();
+                    }
+                }
+                for(int i = 1; i <= d.endBedLocation; i++){
+                    if(!colorBed.containsKey(i)){
+                        col.put(d.color,d.id);
+                        colorBed.put(i,col);
+                        col = new HashMap<>();
+                    }
+                }
+            }
+        }
+        return colorBed;
+    }
+    public Map<Integer,Map<String,Integer>> getTheColorOfBedByNoOffers(){
+        Map<Integer,Map<String,Integer>> colors = getTheColorOfTheBeds();
+        Set<Integer> flowerbeds = generateTheFlowerbeds();
+        Map<String,Integer> beds = new HashMap<>();
+
+        for(Integer i : flowerbeds){
+            if(!colors.containsKey(i)){
+                beds.put("#",0);
+                colors.put(i,beds);
+            }
+        }
+        return colors;
     }
 }
--- src/main/java/practice/complextask/flowerbeds/FlowerBedsMain.java
+++ src/main/java/practice/complextask/flowerbeds/FlowerBedsMain.java
@@ -1,11 +1,14 @@
 package practice.complextask.flowerbeds;
 
 import practice.a_common.MyFileReader;
+import practice.a_common.MyFileWriter;
 
 import java.util.List;
+import java.util.Map;
 
 public class FlowerBedsMain {
     static final String FILE_IN = "src/main/java/resources/flowerbeds/felajanlas.txt";
+    static final String FILE_OUT =  "src/main/java/resources/flowerbeds/colors.txt";
     public static void main(String[] args) {
 
         MyFileReader reader = new MyFileReader(FILE_IN);
@@ -21,6 +24,22 @@ public class FlowerBedsMain {
         System.out.println("\nTask 3:");
         System.out.println("Planters on both side of the entrance: " + manager.getPlantersIdOnBothSideOfBeds() );
 
+        System.out.println("\nTask 4:");
+        System.out.println("Please give me the id of bed: ");
+        int bed =100;
+        System.out.println("a; The number of offers, where the requested bed id is included:  " + manager.countOffersIncludedBed(bed));
+        System.out.println("b; The color of the flower bed, if only the first one plants: " + manager.getTheColorOfTheRequestedBedByTheFirstOffer(bed));
+        System.out.println("c; The color of the bed: " + manager.getTheColorOfTheRequestedBedByAllOffer(bed));
 
+        System.out.println("\nTask 5:");
+        System.out.println(manager.isPossibleToPlantAllBeds());
+
+        System.out.println("\nTask 6:");
+        MyFileWriter writer = new MyFileWriter(FILE_OUT);
+        Map<Integer, Map<String,Integer>> colors = manager.getTheColorOfBedByNoOffers();
+
+        for(Integer i : colors.keySet()){
+            writer.writeIntoFile(colors.get(i) + "\n");
+        }
     }
 }
